b49ad45883b716d2b676d9d6828064de1aa451a6
Lorrayne Firme <lorraynefirme@gmail.com>
2023-03-15 14:56:25 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const livros = [
+    {id: 1, titulo: "Senhor dos aneis"},
+    {id: 2, titulo: "O Hobbit"},
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de Node')
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(200).send("O livro foi cadastrado com sucesso")
+})
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id);
+}
+
+export default app
